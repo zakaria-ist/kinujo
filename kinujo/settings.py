83108ce32ff3879db82ede3d7b9f9a4d03d40b94
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-10-11 13:51:43 +0800

    Api Model Data Modified

--- kinujo/settings.py
+++ kinujo/settings.py
@@ -30,7 +30,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kinujo_local_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
