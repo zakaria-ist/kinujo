5145915b02373873d22c314a131fff34a872cd79
psayeed1990 <sayeedmondal1412@gmail.com>
2020-10-14 08:29:39 +0600

    ui of variants done

--- kinujo/settings.py
+++ kinujo/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kinujo_local_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456Sayeed',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
