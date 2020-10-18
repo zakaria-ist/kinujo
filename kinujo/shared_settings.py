15c6dad8a34ff5b530380d30f21b77e06ab4c6c7
kelvin <kelvin@cloud-consulting.asia>
2020-10-18 22:07:32 +0800

    Temp Update

--- kinujo/shared_settings.py
+++ kinujo/shared_settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kinujo.urls'
