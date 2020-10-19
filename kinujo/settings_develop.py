8d733c1fd88febc56613d999e429a2b5e570e8b3
Mohammad Zakaria <zakaria.ist@gmail.com>
2020-10-19 17:22:36 +0600

    revert

--- kinujo/settings_develop.py
+++ kinujo/settings_develop.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
