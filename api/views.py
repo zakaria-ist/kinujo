2f9a3b42b940b52abf629eb4d157dfae8cd9e8d8
Mohammad Zakaria <zakaria.ist@gmail.com>
2020-10-19 18:38:48 +0600

    language bug fix

--- api/views.py
+++ api/views.py
@@ -294,6 +294,5 @@ def change_language(request):
     deactivate_all()
     activate(language)
     request.session[translation.LANGUAGE_SESSION_KEY] = language
-    # request.LANGUAGE_CODE = translation.get_language()
-    # print('translation.get_language()', translation.get_language())
+    request.LANGUAGE_CODE = translation.get_language()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
