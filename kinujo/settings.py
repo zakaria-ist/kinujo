d46edd593b1d1f9fdb8309e7bc5c5465ed0bf4e3
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-11-02 16:20:04 +0800

    Updated API

--- kinujo/settings.py
+++ kinujo/settings.py
@@ -28,28 +28,28 @@ DEBUG = True
 ALLOWED_HOSTS = ['*', '127.0.0.1', 'kinujo-develop.c2sg.asia',
                  'demo.cl91mjsmzfrn.ap-southeast-1.rds.amazonaws.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kinujo_develop_db',
-        'USER': 'kinujo_develop',
-        'PASSWORD': 'Pa55w0rd',
-        'HOST': 'demo.cl91mjsmzfrn.ap-southeast-1.rds.amazonaws.com',
-        'PORT': '3306',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'kinujo_local_db',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
+#         'NAME': 'kinujo_develop_db',
+#         'USER': 'kinujo_develop',
+#         'PASSWORD': 'Pa55w0rd',
+#         'HOST': 'demo.cl91mjsmzfrn.ap-southeast-1.rds.amazonaws.com',
 #         'PORT': '3306',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kinujo_local_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
