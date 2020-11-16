470c221e2139e1c8f48eeff4dc3c0d8788178ba7
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-11-16 20:56:59 +0800

    Profile Authority

--- api/serializers.py
+++ api/serializers.py
@@ -45,6 +45,7 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     password = serializers.CharField(write_only=True)
     profit = serializers.SerializerMethodField()
     image = ImageSerializer(required=False)
+    authority = AuthoritySerializer()
     # introducer = ProfileSerializer(required=False)
 
     class Meta:
