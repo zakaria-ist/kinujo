d26cf3079245b2a7d11237580f033b0ceb3f0345
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-11-18 17:49:18 +0800

    Added Pay API

--- api/views.py
+++ api/views.py
@@ -470,21 +470,22 @@ class Pay(APIView):
             if profile:
                 profileSerializer = ProfileSerializer(profile, context=getContext())
                 payload = profileSerializer.data['payload']
-                if payload and payload is not None:
-                    payload = payload.replace("'", '"')
-                    payload = json.loads(payload)
+                payload = payload.replace("'", '"')
+                # if payload and payload is not None:
+                #     payload = payload.replace("'", '"')
+                #     payload = json.loads(payload)
 
-                if  payload and payload is not None and 'customerId' in payload:
-                    customer_id = payload["customerId"]
-                else:
-                    customer = stripe.Customer.create(
-                        description=profileSerializer.data['id'],
-                    )
-                    customer_id = customer.id
+                # if  payload and payload is not None and 'customerId' in payload:
+                #     customer_id = payload["customerId"]
+                # else:
+                #     customer = stripe.Customer.create(
+                #         description=profileSerializer.data['id'],
+                #     )
+                #     customer_id = customer.id
 
-                    payload["customerId"] = customer_id
-                    profile.payload = json.dumps(payload)
-                    profile.save()
+                #     payload["customerId"] = customer_id
+                #     profile.payload = json.dumps(payload)
+                #     profile.save()
             return Response({"success" : True, "params" : payload})
         except Exception as e:
             return Response({"success" : False, "error": str(e)}, status=status.HTTP_200_OK)
