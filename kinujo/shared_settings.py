7c5ba9804365fc87adece5bf2d8a29187f4c9dd7
Mohammad Zakaria <zakaria.ist@gmail.com>
2020-11-18 18:11:35 +0600

    Minor update

--- kinujo/shared_settings.py
+++ kinujo/shared_settings.py
@@ -59,6 +59,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -67,7 +68,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kinujo.urls'
