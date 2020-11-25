dd679a03e3cf430b9bf15ac018bb4287bbed5b49
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-11-25 15:11:25 +0800

    Create Product

--- api/insertSerializers.py
+++ api/insertSerializers.py
@@ -82,7 +82,7 @@ class InsertProductImageSerializer(serializers.HyperlinkedModelSerializer):
 class InsertProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ['url', 'id', 'name','brand_name','pr','url_str','category','variety','is_used','is_opened','opened_date','target','price','store_price','shipping_fee','description','is_draft','is_food','is_hidden','created','modified', 'user', 'productImages']
+        fields = ['url', 'id', 'name','brand_name','pr','url_str','category','variety','is_used','is_opened','opened_date','target','price','store_price','shipping_fee','description','is_draft','is_food','is_hidden','created','modified', 'user']
 class InsertProductJancodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductJancode
