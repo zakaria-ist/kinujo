a0d5d373edf2cb65bd71f84ad01bd563385fff30
Lee Guan Chuan <guanchuan@Lees-MacBook-Pro.local>
2020-11-28 20:18:20 +0800

    OrderProduct

--- api/views.py
+++ api/views.py
@@ -652,7 +652,7 @@ class Pay(APIView):
                             'product_name' : product['name'],
                             'shop_name' : shop_name,
                             'address' : addressSerializer.data['address1'],
-                            'order_id' : newOrder.id,
+                            'order_id' : str(orderSerializer.data['id']),
                             'payment' : charge['id']
                         }
                         orderReceiptSerializer = OrderReceiptSerializer(data=orderReceipt, context=getContext())
