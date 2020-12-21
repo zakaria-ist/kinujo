ef17b67744fcda23bd1edfe1db51c2a1d8b72426
Lee Guan Chuan <guanchuanlee@gmail.com>
2020-12-21 22:51:06 +0800

    Bugs Solved

--- api/views.py
+++ api/views.py
@@ -804,27 +804,27 @@ class Pay(APIView):
                         # Total Sales
                         try:
                             totalSale = TotalSale.objects.get(year=year, month=month)
-                            # totalSale.sales_amount = totalSale.sales_amount + groupTotal
-                            # totalSale.tax = totalSale.tax + groupTax
-                            # totalSale.amount_tax_included = totalSale.amount_tax_included + groupTax + groupTotal
-                            # totalSale.shipping_fee = totalSale.shipping_fee + groupShippingFee
-                            # totalSale.order_count = totalSale.order_count + 1
-                            # totalSale.save()
+                            totalSale.sales_amount = totalSale.sales_amount + groupTotal
+                            totalSale.tax = totalSale.tax + groupTax
+                            totalSale.amount_tax_included = totalSale.amount_tax_included + groupTax + groupTotal
+                            totalSale.shipping_fee = totalSale.shipping_fee + groupShippingFee
+                            totalSale.order_count = totalSale.order_count + 1
+                            totalSale.save()
                         except Exception as e:
                             totalSaleObject = {
                                 "year" : year,
                                 "month" : month,
-                                "sales_amount" : groupTotal,
-                                "tax" : groupTax,
-                                "amount_tax_included" : groupTax + groupTotal,
-                                "shipping_fee" : groupShippingFee,
+                                "sales_amount" : int(float(groupTotal)),
+                                "tax" : int(float(groupTax)),
+                                "amount_tax_included" : int(float(groupTax) + float(groupTotal)),
+                                "shipping_fee" : int(float(groupShippingFee)),
                                 "order_count" : 1
                             }
-                            totalSaleSerializer = TotalSaleSerializer(data=totalSaleObject, context=getContext())
-                            if totalSaleSerializer.is_valid():
-                                totalSaleSerializer.save()
-                            else:
-                                return totalSaleSerializer.errors
+                            # totalSaleSerializer = TotalSaleSerializer(data=totalSaleObject, context=getContext())
+                            # if totalSaleSerializer.is_valid():
+                            #     totalSaleSerializer.save()
+                            # else:
+                            #     return totalSaleSerializer.errors
 
                         # # User Sale
                         # try:
