abec3b5610a95c1c79c7c6ae74a4e80ea8eb9660
kelvin <kelvin@cloud-consulting.asia>
2021-01-04 09:03:40 +0800

    Temporary Changed to the S3

--- kinujo/settings_production.py
+++ kinujo/settings_production.py
@@ -162,17 +162,26 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # AWS_SECRET_ACCESS_KEY = 'rLeIDu1n0B6yFN14nIC2e7wykmMca26kMOng8pdV'
 # AWS_STORAGE_BUCKET_NAME = 'kinujoprod'
 # S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# 
+# DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorage'
+# MEDIA_DIRECTORY = '/production/media/'
+# MEDIA_URL = S3_URL + MEDIA_DIRECTORY
+# MEDIA_ROOT = MEDIA_URL
+# WEB_URL = MEDIA_URL
+
 # s3 settings
 AWS_ACCESS_KEY_ID = 'AKIA4YVCJSZFRIFD44QQ'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'c2sg-kinujo'
 S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorage'
-MEDIA_DIRECTORY = '/production/media/'
+DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorageRelease'
+MEDIA_DIRECTORY = '/release/media/'
 MEDIA_URL = S3_URL + MEDIA_DIRECTORY
 MEDIA_ROOT = MEDIA_URL
 WEB_URL = MEDIA_URL
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
