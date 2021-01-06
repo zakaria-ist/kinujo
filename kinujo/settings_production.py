c6e5c72f7119b5e8ba84b72684ef1a5e6da8113b
kelvin <kelvin@cloud-consulting.asia>
2021-01-06 17:49:56 +0800

    no message

--- kinujo/settings_production.py
+++ kinujo/settings_production.py
@@ -157,26 +157,14 @@ STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
-# # s3 settings production
-# AWS_ACCESS_KEY_ID = 'AKIARZPDVQAN2CBCEWRS'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'kinujoprod'
-# S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# 
-# DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorage'
-# MEDIA_DIRECTORY = '/production/media/'
-# MEDIA_URL = S3_URL + MEDIA_DIRECTORY
-# MEDIA_ROOT = MEDIA_URL
-# WEB_URL = MEDIA_URL
-
-# s3 settings
-AWS_ACCESS_KEY_ID = 'AKIA4YVCJSZFRIFD44QQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'c2sg-kinujo'
+# s3 settings production
+AWS_ACCESS_KEY_ID = 'AKIARZPDVQAN2CBCEWRS'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'kinujoprod'
 S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorageRelease'
-MEDIA_DIRECTORY = '/release/media/'
+DEFAULT_FILE_STORAGE = 'utilities.s3utils.MediaS3BotoStorage'
+MEDIA_DIRECTORY = '/production/media/'
 MEDIA_URL = S3_URL + MEDIA_DIRECTORY
 MEDIA_ROOT = MEDIA_URL
 WEB_URL = MEDIA_URL
