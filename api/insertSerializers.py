115da07a27472f7c86f2fe4441b75c71bf4b04c7
kelvin <kelvin@cloud-consulting.asia>
2021-02-11 17:20:15 +0800

    no message

--- api/insertSerializers.py
+++ api/insertSerializers.py
@@ -112,7 +112,7 @@ class InsertOrderSerializer(serializers.HyperlinkedModelSerializer):
     address2 = serializers.CharField(
         allow_blank=True
     )
-    shipped_date = serializers.CharField(allow_null=True)
+    shipped_date = serializers.CharField(allow_blank=True, allow_null=True)
     class Meta:
         model = Order
         fields = ['id', 'url', 'seller','purchaser','amount','tax','shipping_fee','total_amount','name','zip1','prefecture','address1','address2','tel','payment','customer_remark','remark','is_hidden','created','modified', 'status', 'tel_code', 'shipped_date']
