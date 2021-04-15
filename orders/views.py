a51a22402b611cf8b3f326d75f6b008029ec113c
Mohammad Zakaria Sarker <zakaria.ist@gmail.com>
2021-04-15 15:31:31 +0600

    order detail bug fix

--- orders/views.py
+++ orders/views.py
@@ -577,8 +577,8 @@ def order_add(request):
 
         orderer_list = list(Profile.objects.filter(is_hidden=False,
                     authority_id__in=[AUTHORITY_TYPE['STORE'], AUTHORITY_TYPE['GENERAL']])\
-            .exclude(id=seller_id)\
             .values_list('id', 'real_name', 'authority_id', 'nickname', 'is_seller', 'is_approved'))
+            # .exclude(id=seller_id)\
         prefecture_list = list(Prefecture.objects.filter(is_hidden=False, is_enable=True).order_by('id').values_list('id', 'name'))
         tel_code_list = CountryCode.objects.filter(is_hidden=False).values('id', 'name', 'tel_code')
         if request.LANGUAGE_CODE == 'en':
@@ -812,8 +812,8 @@ def order_edit(request, order_id):
 
             orderer_list = list(Profile.objects.filter(is_hidden=False,
                         authority_id__in=[AUTHORITY_TYPE['STORE'], AUTHORITY_TYPE['GENERAL']])\
-                .exclude(id=seller_id)\
                 .values_list('id', 'real_name', 'authority_id', 'nickname', 'is_seller', 'is_approved'))
+                # .exclude(id=seller_id)\
             prefecture_list = list(Prefecture.objects.filter(is_hidden=False, is_enable=True).order_by('id').values_list('id', 'name'))
             if request.LANGUAGE_CODE == 'en':
                 status_list = ORDER_STATUS
