473270068af7e1a0821aa4d4b717d79b170823c0
OCEAN2012\kana_mihata <kana_mihata@oceansoftware.co.jp>
2021-10-08 15:05:12 +0900

    import datetime

--- kinujo/settings_release.py
+++ kinujo/settings_release.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 # from .shared_settings import *
 import os
+import datetime
 from django.utils.translation import ugettext_lazy as _
+from datetime import timedelta
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
